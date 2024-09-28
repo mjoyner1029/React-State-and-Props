d0e854f066712a16e11995c2062d060aebb2e978
Miles Joyner <62813978+mjoyner1029@users.noreply.github.com>
2024-09-27 17:09:06 -0700

    Update UserProfile.jsx

--- my-react-app/src/components/UserProfile.jsx
+++ my-react-app/src/components/UserProfile.jsx
@@ -3,20 +3,35 @@ import React, { Component } from 'react';
 class UserProfile extends Component {
     constructor(props) {
         super(props);
-        this.state = { name: 'Alex' };
+        this.state = { name: 'Alex', editMode: false };
         this.changeName = this.changeName.bind(this);
+        this.toggleEditMode = this.toggleEditMode.bind(this);
     }
 
     changeName() {
         this.setState({ name: 'Charlie' });
     }
 
+    toggleEditMode() {
+        this.setState(prevState => ({ editMode: !prevState.editMode }));
+    }
+
     render() {
         return (
             <div>
                 <h1>User Profile</h1>
-                <p>Name: {this.state.name}</p>
-                <button onClick={this.changeName}>Change Name</button>
+                {this.state.editMode ? (
+                    <div>
+                        <p>Edit Mode: Change your name</p>
+                        <button onClick={this.changeName}>Change Name</button>
+                        <button onClick={this.toggleEditMode}>Save</button>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Name: {this.state.name}</p>
+                        <button onClick={this.toggleEditMode}>Edit</button>
+                    </div>
+                )}
             </div>
         );
     }
