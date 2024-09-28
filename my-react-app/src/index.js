593bc7ba4118c7a6b6b669d80e579e6c5eed30bf
Miles Joyner <62813978+mjoyner1029@users.noreply.github.com>
2024-09-27 17:09:52 -0700

    Update index.js

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Optional: add a CSS file for styles
+import './index.css'; // Link to the CSS file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
